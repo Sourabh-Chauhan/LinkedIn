7f234e742e9ad7d6c37be7f9cbfc3494158ef9ad
Sourabh-Chauhan <sourabhc2018@gmail.com>
2025-11-23 01:34:53 +0530

    Add: Feed, left, right SideBar and Create Post Modal

---
+++ frontend/src/features/feed/components/LeftSidebar/LeftSidebar.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import classes from "./LeftSidebar.module.scss";
+import type { IUser } from "../../../authentication/contexts/AuthenticationContextProvider";
+import { useNavigate } from "react-router-dom";
+
+interface ILeftSidebarProps {
+  user: IUser | null;
+}
+
+function LeftSidebar({ user }: ILeftSidebarProps) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.cover}>
+        <img
+          src={
+            user?.coverPicture
+              ? `${import.meta.env.VITE_API_URL}/api/v1/storage/${
+                  user?.coverPicture
+                }`
+              : "/cover.jpeg"
+          }
+          alt="Cover"
+        />
+      </div>
+      <button
+        className={classes.avatar}
+        onClick={() => navigate("/profile/" + user?.id)}
+      >
+        <img
+          src={
+            user?.profilePicture
+              ? `${import.meta.env.VITE_API_URL}/api/v1/storage/${
+                  user?.profilePicture
+                }`
+              : "/avatar.svg"
+          }
+          alt=""
+        />
+      </button>
+
+      <div className={classes.name}>
+        {user?.firstName + " " + user?.lastName}
+      </div>
+      <div className={classes.title}>
+        {user?.position + " at " + user?.company}
+      </div>
+      <div className={classes.info}>
+        <div className={classes.item}>
+          <div className={classes.label}>Profile viewers</div>
+          <div className={classes.value}>0</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LeftSidebar;
---
+++ frontend/src/features/feed/components/RightSidebar/RightSidebar.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import classes from "./RightSidebar.module.scss";
+import type { IUser } from "../../../authentication/contexts/AuthenticationContextProvider";
+import { useNavigate, useParams } from "react-router-dom";
+import { request } from "../../../../utils/api";
+import { Loader } from "../../../../components/Loader/Loader";
+function RightSidebar() {
+    const [suggestions, setSuggestions] = useState<IUser[]>([]);
+    const [loading, setLoading] = useState(true);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    useEffect(() => {
+        request<IUser[]>({
+            endpoint: "/api/v1/networking/suggestions?limit=2",
+            onSuccess: (data) => {
+                if (id) {
+                    setSuggestions(data.filter((s) => s.id !== id));
+                } else {
+                    setSuggestions(data);
+                }
+            },
+            onFailure: (error) => console.log(error),
+        }).then(() => setLoading(false));
+    }, [id]);
+
+    return (
+        <div className={classes.root}>
+            <h3>Add to your connexions</h3>
+            <div className={classes.items}>
+                {suggestions.map((suggestion: IUser) => {
+                    return <div className={classes.item} key={suggestion.id}>
+                        <button className={classes.avatar} onClick={() => navigate("/profile/" + suggestion.id)}>
+                            <img src={suggestion.profilePicture || "/avatar.svg"} alt="" />
+                        </button>
+                    </div>;
+                })}
+            </div>
+            RightSidebar
+
+            {loading && <Loader isInline />}
+        </div>
+    );
+}
+
+export default RightSidebar;
